4cb1b25e6e2972dcaf0fcd731dcc5ac64ef0abd5
Adrian <tommazost@gmail.com>
2017-04-22 17:00:46 +0200

    exercise with commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');
+// have to add comment only english language? alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+/* 
+Coomment bloks in this ang line below
+*/
+
+// Comment inline 
